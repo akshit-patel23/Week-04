690cb83e4d89508ab4a7f7611dd4587cceb1246a
akshit-patel23 <akshitpatel.g@gmail.com>
2025-02-14 10:39:16 +0530

    [Akshit] Add . Smart Warehouse Management System and its testing

---
+++ Day-01/src/test/java/smartwarehousemanagementsystemtest/SmartWarehouseTest.java
@@ -0,0 +1,27 @@
+package smartwarehousemanagementsystemtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import smartwarehousemanagementsystem.Electronics;
+import smartwarehousemanagementsystem.Storage;
+
+public class SmartWarehouseTest {
+    @Test
+    public void tester1(){
+        Electronics television1=new Electronics("Lenovo");
+        Storage<Electronics> storageElectronics=new Storage<>();
+        storageElectronics.addItem(television1);
+        Assert.assertEquals(1,storageElectronics.getList().size());
+    }
+    
+    @Test
+    public void tester2(){
+        Electronics television1=new Electronics("Lenovo");
+        Storage<Electronics> storageElectronics=new Storage<>();
+        storageElectronics.addItem(television1);
+
+        Assert.assertEquals("Lenovo",storageElectronics.getList().get(0).getName());
+
+    }
+
+}
