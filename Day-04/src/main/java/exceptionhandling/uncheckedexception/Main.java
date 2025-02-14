8b999c8efd296e346ad3bb9f3a9d42c96af4c3be
akshit-patel23 <akshitpatel.g@gmail.com>
2025-02-14 16:18:21 +0530

    [Akshit] Add .  Checked Exception (Compile-time Exception)

--- Day-04/src/main/java/exceptionhandling/uncheckedexception/Main.java
+++ Day-04/src/main/java/exceptionhandling/uncheckedexception/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
        int num1=sc.nextInt();
+
        int num2=sc.nextInt();
+
 
         Unchecked uc= new Unchecked();
         uc.divide(num1,num2);
