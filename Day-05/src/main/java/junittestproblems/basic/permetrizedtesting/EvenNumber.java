2015503d0668acccbe78fb6dd57759e4cf07763c
akshit-patel23 <akshitpatel.g@gmail.com>
2025-02-16 16:53:56 +0530

    [Akshit] Add . Testing Parameterized Tests with Junit testing

---
+++ Day-05/src/main/java/junittestproblems/basic/permetrizedtesting/EvenNumber.java
@@ -0,0 +1,9 @@
+package junittestproblems.basic.permetrizedtesting;
+
+public class EvenNumber {
+      public static boolean isEven(int number) {
+            return number % 2 == 0;
+        }
+    }
+
+
